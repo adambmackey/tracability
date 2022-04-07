5d9bc74f6cc441f66cc1cd09add0526e93ffda82
Adam Mackey <adambmackey@gmail.com>
2022-04-07 15:01:16 -0400

    2

--- server/server.js
+++ server/server.js
@@ -3,6 +3,18 @@ const path = require('path')
 
 const app = express()
 
+// include and initialize the rollbar library with your access token
+var Rollbar = require("rollbar");
+var rollbar = new Rollbar({
+  accessToken: '7c342300cffb426e9a9111261232da32',
+  captureUncaught: true,
+  captureUnhandledRejections: true
+});
+
+// record a generic message and send it to Rollbar
+rollbar.log("Hello world!");
+
+
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
